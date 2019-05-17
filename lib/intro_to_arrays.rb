334386a3a2fd13be2445c2d8a38dee367390c20a
Sam Markham <sam.e.markham@gmail.com>
2019-05-17 21:51:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,11 @@
-@my_new_array = []
+#@my_new_array = []
 
-@my_two_arrays = ["chocolate", "cake"]
+#@my_two_arrays = ["chocolate", "cake"]
+
+def instantiate_new_array 
+  @my_new_array = []
+end 
+
+def array_with_two_elements 
+  @my_two_array = ["supergirl", "martian manhunter"]
+end 
