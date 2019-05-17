887072134129af22f64c2b6c891fe2de043afa18
Sam Markham <sam.e.markham@gmail.com>
2019-05-17 22:02:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -13,3 +13,15 @@ end
 def third_element(array)
   array[2]
 end 
+
+def last_element(array)
+  array[-1]
+end 
+
+def first_element_with_array_methods 
+  
+end 
+
+def last_element_with_array_methods
+  
+end 
