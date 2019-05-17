3e692ec1b15f064eb905cb0a8a139008a499f94d
Sam Markham <sam.e.markham@gmail.com>
2019-05-17 21:51:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,7 +1,3 @@
-#@my_new_array = []
-
-#@my_two_arrays = ["chocolate", "cake"]
-
 def instantiate_new_array 
   @my_new_array = []
 end 
@@ -9,3 +5,4 @@ end
 def array_with_two_elements 
   @my_two_array = ["supergirl", "martian manhunter"]
 end 
+
